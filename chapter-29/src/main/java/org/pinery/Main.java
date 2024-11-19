34cc462d800ecc35877f07af65ea70bb1dbd833f
pxsbest <pxsbest@163.com>
2024-11-19 14:04:10 +0800

    Event Driven 设计模式

---
+++ chapter-29/src/main/java/org/pinery/Main.java
@@ -0,0 +1,15 @@
+package org.pinery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author gensci
+ * @DateTime 2024/10/28 11:28
+ * @Description
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
